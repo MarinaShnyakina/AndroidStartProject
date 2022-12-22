ebc5de94ba75d55bcd82e4d219b2df6b575096cc
Marina Shnyakina <schnyakina@yandex.ru>
2022-12-22 11:31:32 +0300

    The second version of the calculator and Logcat

--- app/src/main/java/ru/synergy/androidstartproject/Calculator.java
+++ app/src/main/java/ru/synergy/androidstartproject/Calculator.java
@@ -1,12 +1,17 @@
 package ru.synergy.androidstartproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cursoradapter.widget.SimpleCursorAdapter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,13 +19,31 @@ import android.widget.Toast;
 public class Calculator extends AppCompatActivity {
 
     private static final String LogcatTag = "CALCULATOR_ACTIVITY";
+    private static final String LifecycleTag = "LIFECYCLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LifecycleTag, "I'm onCreate(), and I'm started");
         setContentView(R.layout.activity_calculator);
 
         final Button calculate = (Button) findViewById(R.id.calc);
+
+        /* Context training
+                TextView textView = new TextView(this);
+        ListAdapter adapter = new;
+        SimpleCursorAdapter(getApplicationContext(),);
+
+        // Доступ из класса Activity - наследник Context
+        getSystemService(LAYOUT_INFLATER_SERVISE);
+
+        // Shared prefs - доступ с использованием контекста приложения
+
+        SharedPreferences prefs =
+                getApplicationContext().getSharedPreferences("PREFS", MODE_PRIVATE);
+        */
+
+        // intent - посылка
 
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +52,36 @@ public class Calculator extends AppCompatActivity {
                 calculateAnswer();
             }
         });
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LifecycleTag, "I'm onStart(), and I'm started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LifecycleTag, "I'm onStop and I'm started");
+    }
+
+    @Override
+    protected void onDestroy() {
+    super.onDestroy();
+        Log.d(LifecycleTag, "I'm onDestroy and I'm started");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LifecycleTag, "I'm onPause and I'm started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LifecycleTag, "I'm onResume() and I'm started");
     }
 
     private void calculateAnswer() {
@@ -41,31 +93,45 @@ public class Calculator extends AppCompatActivity {
         RadioButton multiply = (RadioButton) findViewById(R.id.multiply);
         RadioButton divide = (RadioButton) findViewById(R.id.divide);
 
+        numOne.setText("0");
+        numTwo.setText("0");
+        add.setChecked(true);
+
         TextView answer = (TextView) findViewById(R.id.result);
 
         Log.d(LogcatTag, "All views have been founded");
 
-        float numone = Integer.parseInt(numOne.getText().toString());
-        float numtwo = Integer.parseInt(numTwo.getText().toString());
+        float numone = 0;
+        float numtwo = 0;
+        String num1 = numOne.getText().toString();
+        String num2 = numTwo.getText().toString();
+
+        if(num1.equals("") && num1 != null) {
+            numone = Integer.parseInt(numOne.getText().toString());
+        }
+
+        if(num2.equals("") && num2 != null) {
+            numtwo = Integer.parseInt(numTwo.getText().toString());
+        }
 
         Log.d(LogcatTag, "Successfully grabbed date from input fields");
         Log.d(LogcatTag, "numone is: " + numone + " ; "+" numtwo is: " + numtwo);
 
-        float result = 0;
+        float solution = 0;
 
         if(add.isChecked()){
             Log.d(LogcatTag, "Opetation is add");
-            result = numone + numtwo;
+            solution = numone + numtwo;
         }
 
         if(subtract.isChecked()){
             Log.d(LogcatTag, "Opetation is subtract");
-            result = numone - numtwo;
+            solution = numone - numtwo;
         }
 
         if(multiply.isChecked()){
             Log.d(LogcatTag, "Opetation is multiply");
-            result = numone * numtwo;
+            solution = numone * numtwo;
         }
 
         if(divide.isChecked()){
@@ -74,11 +140,12 @@ public class Calculator extends AppCompatActivity {
                 Toast.makeText(this, "Number two Cannot be zero", Toast.LENGTH_SHORT).show();
                 return;
             }
-            result = numone / numtwo;
+            solution = numone / numtwo;
         }
 
-        Log.d(LogcatTag, "The result of operations is: " + result);
-        answer.setText("The answer is " + result);
+        Log.d(LogcatTag, "The result of operations is: " + solution);
+
+        answer.setText("The answer is " + solution);
 
     }
 }
